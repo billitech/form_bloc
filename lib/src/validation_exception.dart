284d4357d81d9a3633fc5d268c673ec833629c75
Okon Sylvester Charlie <silverprince4134u@gmail.com>
2022-03-06 21:44:24 +0100

    bug fix

--- lib/src/validation_exception.dart
+++ lib/src/validation_exception.dart
@@ -1,7 +1,15 @@
-class FormValidationException {
+import 'package:equatable/equatable.dart';
+
+class FormValidationException extends Equatable{
 
   final Map<String, String> error;
   final String  message;
 
   const FormValidationException({required this.error, required this.message});
+
+  @override
+  List<Object?> get props => [error, message];
+
+  @override
+  String toString() => "FormValidationException { message: $message, error: $error}";
 }
